502bd5153761c7b168000c3396020eac8a054db9
Daniel M Brasil <danielmbrasil@protonmail.com>
2025-09-05 16:16:13 -0300

    reorganize js
    Co-authored-by: João Victor Malheiros Tenório <jvictormalheiros@gmail.com>
    Co-authored-by: Gabriel Quaresma <j.quaresmasantos_98@hotmail.com>

---
+++ client/js/tracks.js
@@ -0,0 +1,50 @@
+const fetchTracks = async () => {
+  const response = await fetch("http://localhost:4567/songs");
+  return await response.json();
+};
+
+const fetchTrack = async (id) => {
+  const response = await fetch(`http://localhost:4567/songs/${id}/audio`);
+  return response;
+};
+
+function htmlToNode(html) {
+  const template = document.createElement("template");
+  template.innerHTML = html;
+  const nNodes = template.content.childNodes.length;
+  if (nNodes !== 1) {
+    throw new Error(
+      `html parameter must represent a single node; got ${nNodes}. ` +
+        "Note that leading or trailing spaces around an element in your " +
+        'HTML, like " <img/> ", get parsed as text nodes neighbouring ' +
+        "the element; call .trim() on your input to avoid this."
+    );
+  }
+  return template.content.firstChild;
+}
+
+const response = await fetchTracks();
+
+const tracksGrid = document.querySelector(".tracks-grid");
+
+response.forEach((track) => {
+  const htmlStr = `
+    <div class="track-card">
+      <img class="track-image" src="https://static.wikia.nocookie.net/beatles/images/8/82/Thebeatlesabbeyroad.jpg" alt="Track Title">
+      <div class="track-info">
+        <h3 class="track-title">${track.title}</h3>
+        <p class="track-artist">${track.artist}</p>
+      </div>
+    </div>
+`;
+
+  const div = htmlToNode(htmlStr.trim());
+
+  tracksGrid.appendChild(div);
+});
+
+
+// interface volume e progresso mobile
+// mudar o audio selecionado com base no click
+// separar arquivos js por contextos e chamar individualmente no index.html
+// opcional: conseguir setar o tempo na interface de progresso
